8831a9885c54de39a05c8c5dc1ae10c65652323d
RobertoRuben <robertoch263@gmail.com>
2025-10-06 09:14:09 -0500

    🎉 feat(api): add return type hints for projection sheet and varieties count data methods

--- src/api/interface/extractor_api.py
+++ src/api/interface/extractor_api.py
@@ -3,17 +3,17 @@ from abc import ABC, abstractmethod
 
 class ExtractorAPI(ABC):
     @abstractmethod
-    async def get_piquillo_projection_sheet_data(self, date: str):
+    async def get_piquillo_projection_sheet_data(self, date: str) -> dict[str, str]:
         pass
 
     @abstractmethod
-    async def get_california_projection_sheet_data(self, date: str):
+    async def get_california_projection_sheet_data(self, date: str) -> dict[str, str]:
         pass
 
     @abstractmethod
-    async def get_piquillo_varieties_count_data(self, date: str):
+    async def get_piquillo_varieties_count_data(self, date: str) -> dict[str, str]:
         pass
 
     @abstractmethod
-    async def get_california_varieties_count_data(self, date: str):
+    async def get_california_varieties_count_data(self, date: str) -> dict[str, str]:
         pass
